7437f6287692d893b88d82ec4ce218c638276c95
caksa87 <caksakdr@gmail.com>
2021-02-19 18:46:37 +0700

    [UPDATE] FeedCreate : unify textarea and dropzone

--- src/components/pages/feed/FeedCreate.vue
+++ src/components/pages/feed/FeedCreate.vue
@@ -17,8 +17,13 @@
           </div>
         </div>
 
-        <div class="feed-content">
-          <div id="editor">
+        <div
+          class="feed-content"
+          @dragenter="hideTa = true, hideDropzone = false"
+          @dragover.prevent="checkDragOver">
+          <div
+            id="editor"
+            :class="{ 'hidden' : hideTa }">
             <at-ta
               :members="peoples"
               name-key="full_name"
@@ -38,6 +43,7 @@
                 </div>
               </template>
               <textarea
+                ref="caption"
                 v-model="inputCreate"
                 @keyup.enter.ctrl="createNewFeed()"
                 placeholder="Type here ..."
@@ -50,11 +56,18 @@
             class="content-create-image"
             :options="inputCreateImageOptions"
             :duplicateCheck="true"
-            @vdropzone-thumbnail="inputCreateImageDataUrl"></vue-dropzone>
+            @dragover.prevent="checkDragOver"
+            @vdropzone-drag-leave="hideTa = false, hideDropzone = true"
+            @vdropzone-drop="hideTa = false, hideDropzone = false"
+            @vdropzone-thumbnail="inputCreateImageDataUrl"
+            :class="{ 'hidden' : hideDropzone }"></vue-dropzone>
         </div>
 
         <div class="feed-action">
-          <button @click="createNewFeed()" class="button ml-auto" title="or Ctrl + Enter to send">
+          <button @click="openDropzone" class="button ml-auto" title="Upload pictures">
+            <image-icon class="icon"/>Pictures
+          </button>
+          <button @click="createNewFeed()" class="button" title="or Ctrl + Enter to send">
             <send-icon class="icon"/>Post
           </button>
         </div>
@@ -69,6 +82,7 @@ import PeopleAvatar from '@/components/widgets/PeopleAvatar'
 import vue2Dropzone from 'vue2-dropzone'
 import 'vue2-dropzone/dist/vue2Dropzone.min.css'
 import {
+  ImageIcon,
   SendIcon
 } from 'vue-feather-icons'
 
@@ -86,11 +100,14 @@ export default {
     AtTa,
     PeopleAvatar,
     vueDropzone: vue2Dropzone,
+    ImageIcon,
     SendIcon
   },
 
   data () {
     return {
+      hideTa: false,
+      hideDropzone: true,
       inputCreate: '',
       img: [],
       inputCreateImageOptions: {
@@ -110,8 +127,21 @@ export default {
   },
 
   methods: {
+    checkDragOver (e) {
+      e.preventDefault()
+      e.stopPropagation()
+      this.hideTa = true
+      this.hideDropzone = true
+    },
+
+    openDropzone () {
+      this.$refs.inputCreateImage.$el.click()
+    },
+
     inputCreateImageDataUrl (file, dataUrl) {
       this.img.push(dataUrl)
+      this.hideTa = false
+      this.hideDropzone = false
     },
 
     createNewFeed (inputCreate) {
@@ -129,8 +159,17 @@ export default {
         this.$refs.inputCreateImage.removeAllFiles()
         this.inputCreate = ''
         this.img = []
+        this.hideTa = false
+        this.hideDropzone = true
+      } else if (!this.inputCreate) {
+        alert('Caption is required')
+        this.$refs.caption.focus()
       }
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .content-create-image { min-height: 170px; }
+</style>
