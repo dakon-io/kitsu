8813b09757d49f8513db5f6c99aa80d1b057007e
caksa87 <caksakdr@gmail.com>
2021-03-09 18:16:21 +0700

    [ADD] new component : InvoiceTag

---
+++ src/components/widgets/InvoiceTag.vue
@@ -0,0 +1,22 @@
+<template>
+  <span
+    class="tag"
+    :class="[status == 'waiting' ? 'orange' : status == 'paid' ? 'green' : status == 'approved' ? 'blue' : status == 'rejected' ? 'red' : '']">
+    {{ status == 'waiting' ? $t('invoices.status.ongoing') : status == 'paid' ? $t('invoices.status.paid') : status == 'approved' ? $t('invoices.status.approved') : status == 'rejected' ? $t('invoices.status.rejected') : '' }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'InvoiceTag',
+
+  props: {
+    status: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
